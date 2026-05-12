689648f2018f62200bb93515b87ddf199c012377
Sanjida Parven Alfe <sanjidaparvinalfe@gmail.com>
2026-05-13 02:18:57 +0600

    fix deployment configuration

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,11 @@ app.get('/', (req, res) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
